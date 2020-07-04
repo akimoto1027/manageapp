65925b1b3b3bd92679b22d22066f0771750567e8
akimoto1027 <64831697+akimoto1027@users.noreply.github.com>
2020-07-04 15:15:50 +0900

    登録view,ログインviewのリンク付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_04_054106) do
+ActiveRecord::Schema.define(version: 2020_07_04_060625) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "genre"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_07_04_054106) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
